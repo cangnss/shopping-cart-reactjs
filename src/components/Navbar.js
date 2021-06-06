fc9ed90413d59157749b56444d77882833b812b0
Ali Can Güneş <cgunes52@gmail.com>
2021-06-06 20:53:44 +0300

    shopping cart added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,13 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 function Navbar({piece}) {
+
+    const shoppingCard = {
+        color:'gray'
+    };
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">Shopping Cart</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,8 +25,8 @@ function Navbar({piece}) {
                         </ul>
                     </div>
                     <div>
-                        <i className="fas fa-shopping-cart"></i>
-                        <span className="ms-2">Cart({piece})</span>
+                        <i className="fas fa-shopping-cart fa-2x" style={shoppingCard}></i>
+                        <span className="ms-2 fs-5" style={{color:'gray'}}>Cart({piece})</span>
                     </div>
                 </div>
             </nav>
